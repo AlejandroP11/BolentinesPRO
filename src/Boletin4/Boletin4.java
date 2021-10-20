19a7e9a673f497aad644a8c594c5209df14ad101
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2021-10-20 11:08:53 +0200

    Boletin 4 (Modificado)

--- src/Boletin4/Boletin4.java
+++ src/Boletin4/Boletin4.java
@@ -11,14 +11,15 @@ public class Boletin4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Libro objLi1 = new Libro("aaa","aa",2021,(short)450);
+        objLi1.amosar();
         Libro unlibro = new Libro();
         unlibro.setautor("J.K. Rowling");
         unlibro.settitulo("Harry Potter y la Piedra Filosofal");
         unlibro.setaño(1997);
         unlibro.setvaloracion(9.2f);
         unlibro.setPaginas((short) 233);
-        System.out.println("El libro es: "+unlibro.gettitulo()+"\nSu autor es: "+unlibro.getautor()+"\nEl año en el que fue publicado es: "+
-        unlibro.getaño()+"\nTiene "+unlibro.getpaginas()+" páginas \nY su valoración es de: "+unlibro.getvaloracion());
+        unlibro.amosar();
     }
     
 }

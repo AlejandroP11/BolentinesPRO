9f4768213a5b5c36d0cda3221621214cab5ad3d8
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2021-10-27 12:33:16 +0200

    Boletín 7

---
+++ src/Boletin7/boletin7_2/Boletin7_2.java
@@ -0,0 +1,19 @@
+
+package boletin7_2;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin7_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejercicio2 obj1=new Ejercicio2();
+        obj1.numRe();
+
+    }
+    
+}
---
+++ src/Boletin7/boletin7_3/Boletin7_3.java
@@ -0,0 +1,18 @@
+
+package boletin7_3;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin7_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejercicio3 obj1=new Ejercicio3();
+        obj1.numdi();
+    }
+    
+}

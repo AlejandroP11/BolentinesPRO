e38f3e0df6d59cd965abcd9da45da0e07bb5ad40
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2021-10-28 10:07:56 +0200

    Boletín 7.4

---
+++ src/Boletin7/boletin7_4/Boletin7_4.java
@@ -0,0 +1,19 @@
+
+package boletin7_4;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin7_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejercicio4 obj1=new Ejercicio4();
+        obj1.darDatos();
+ 
+    }
+    
+}
---
+++ src/Boletin7/boletin7_4/Ejercicio4.java
@@ -0,0 +1,35 @@
+
+package boletin7_4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dam1
+ */
+public class Ejercicio4 {
+    Scanner sc;
+    public void darDatos(){
+        String nombreA=leerNombre();
+        float pesoA=leerPeso();
+        String nombreB=leerNombre();
+        float pesoB=leerPeso();
+        
+        if(pesoA>pesoB)
+            System.out.println(nombreA+" es la persona más pesada con "+pesoA+"kilos y la diferencia de peso es de "+(pesoA-pesoB)+" kilos.");
+        else 
+            System.out.println(nombreB+" es la persona más pesada con "+pesoB+"kilos y la diferencia de peso es de "+(pesoB-pesoA)+" kilos.");
+    }
+    public float leerPeso(){
+        sc=new Scanner(System.in);
+        System.out.println("Introduzca su peso");
+        float peso=sc.nextFloat();
+        return peso;
+    }
+    public String leerNombre(){
+        sc=new Scanner(System.in);
+        System.out.println("Introduzca su nombre");
+        String nombre=sc.next();
+        return nombre;
+    }
+}

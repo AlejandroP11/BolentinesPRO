9594018de98fe16f44ed780abd19ddd0282f6214
Alejandro Pereiro G <jpereirogabian@danielcastelao.org>
2021-11-08 21:11:59 +0100

    Boletín 8_2

---
+++ src/Boletin8/Boletin8_2/Boletin8_2.java
@@ -0,0 +1,17 @@
+package boletin8_2;
+
+/**
+ *
+ * @author Alejandro Pereiro G
+ */
+public class Boletin8_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Figura obj1=new Figura();
+        obj1.darSuperficie();
+    }
+    
+}

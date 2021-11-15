c40a06a6e03dd928ae5d7d0e7c92f654ad19eb65
Alejandro Pereiro G <jpereirogabian@danielcastelao.org>
2021-11-15 08:11:54 +0100

    Boletín 9_5

---
+++ src/Boletin9/boletin9_5/Boletin9_5.java
@@ -0,0 +1,18 @@
+
+package boletin9_5;
+
+/**
+ *
+ * @author Alejandro Pereiro G
+ */
+public class Boletin9_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Salarios salario1=new Salarios();
+        salario1.contarSalarios();
+    }
+    
+}
---
+++ src/Boletin9/boletin9_5/Salarios.java
@@ -0,0 +1,38 @@
+
+package boletin9_5;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alejandro Pereiro G
+ */
+public class Salarios {
+    public void contarSalarios(){
+        int i=0;
+        int j=0;
+        int k=0;
+        float por=0;
+        float s=pedirSalario();
+        while(s<0||s>0){
+            if(s<0){
+            }
+            else{
+                i++;
+                if(s>=1000&&s<=1750){
+                    k++;
+                }
+                else if(s<1000){
+                    j++;
+                    por=100*j/i;
+                }
+            }
+            s=pedirSalario();
+        }
+        System.out.println(k+" trabajadores ganan entre 1000 y 1750 euros y el "+por+"% cobran menos de 1000 euros");
+    }
+    public float pedirSalario(){
+        float salario=Float.parseFloat(JOptionPane.showInputDialog("Introduce el salario"));
+        return salario;
+    }
+}

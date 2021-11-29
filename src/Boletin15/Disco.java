63bea3472a83112b024adba9ef7784ce4a81f38a
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2021-11-29 10:22:03 +0100

    Boletin15

---
+++ src/Boletin15/Disco.java
@@ -0,0 +1,23 @@
+
+package Boletin15;
+
+/**
+ *
+ * @author dam1
+ */
+public class Disco extends Multimedia {
+    public String genero;
+    public Disco() {
+    }
+    public Disco(String genero, String titulo, String autor, String formato, int duracion) {
+        super(titulo, autor, formato, duracion);
+        this.genero = genero;
+    }
+    public String getGenero() {
+        return genero;
+    }
+    @Override
+    public String toString() {
+        return super.toString()+ " genero=" + genero ;
+    }       
+}
---
+++ src/Boletin15/Multimedia.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Boletin15;
+
+/**
+ *
+ * @author dam1
+ */
+public class Multimedia {
+    private String titulo;
+    private String autor; 
+    private String formato; 
+    public int duracion;
+    public Multimedia() {
+    }
+    public Multimedia(String titulo, String autor, String formato, int duracion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.formato = formato;
+        this.duracion = duracion;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public String getFormato() {
+        return formato;
+    }
+    public int getDuracion() {
+        return duracion;
+    }
+    @Override
+    public String toString() {
+        return "titulo=" + titulo + ", autor=" + autor + ", formato=" + formato + ", duracion=" + duracion;
+    }    
+}

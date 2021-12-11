431d3481eec8d671f1dc38ce8c7ee46cc2fa6e66
Alejandro Pereiro G <jpereirogabian@danielcastelao.org>
2021-12-11 20:20:06 +0100

    Boletin17

---
+++ src/Boletin17/Boletin17.java
@@ -0,0 +1,18 @@
+
+package Boletin17;
+
+/**
+ *
+ * @author Alejandro Pereiro G
+ */
+public class Boletin17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Notas notas=new Notas();
+        notas.calcularNotaFinal();
+    }
+    
+}

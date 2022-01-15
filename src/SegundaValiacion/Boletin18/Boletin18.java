415a5b515c64258d67a4fa87d1d687c2c6d4decb
Alejandro Pereiro G <jpereirogabian@danielcastelao.org>
2022-01-15 19:24:28 +0100

    Separacion de trimestres

---
+++ src/SegundaValiacion/Boletin18/Boletin18.java
@@ -0,0 +1,25 @@
+
+package SegundaValiacion.Boletin18;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Alejandro Pereiro G
+ */
+public class Boletin18 {
+    
+    public static void main(String[] args){
+        float centigrados=Float.parseFloat(JOptionPane.showInputDialog("Ingresa la temperatura en centigrados"));
+        ConversorTemperaturas conversor=new ConversorTemperaturas();
+        try{
+            conversor.centigradosAFharenheit(centigrados);
+        }catch(TemperaturaErradaException ex){
+            System.out.println(ex.getMessage());
+        }
+        try{
+            conversor.centigradosAReamur(centigrados);
+        }catch(TemperaturaErradaException ex){
+          
+        }
+    }
+    
+}
---
+++ src/SegundaValiacion/Boletin18/TemperaturaErradaException.java
@@ -0,0 +1,15 @@
+
+package SegundaValiacion.Boletin18;
+
+/**
+ *
+ * @author Alejandro Pereiro G
+ */
+public class TemperaturaErradaException extends Exception{
+    public TemperaturaErradaException(){
+        super();
+    }
+    public TemperaturaErradaException(String mensaje){
+        super(mensaje);
+    }
+}

be5c6f81ae9b969b5c2e54b10c3753fa4c61df53
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2022-03-30 12:32:08 +0200

    Boletin28

---
+++ src/TerceraValiacion/Boletin28/Boletin28.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TerceraValiacion.Boletin28;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletin28 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MarcoUno ob = new MarcoUno();
+        ob.setVisible(true);
+    }
+    
+}
